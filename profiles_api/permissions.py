5f4db98c9567d38866ef311f054291dbf97721d2
Theofilus Marcel Irawan Sutomo <50488473+MarcelIrawan@users.noreply.github.com>
2020-09-01 11:05:52 +0700

    Reset database and added user profile feed view set

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,4 +10,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
-        
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow user to edit their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.id == request.user.id
